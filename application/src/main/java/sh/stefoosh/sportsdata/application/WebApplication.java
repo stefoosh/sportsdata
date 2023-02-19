f0306a6c9f3a911c9e7410e3903421c07cbb04b7
stefoosh <stephan.aleman@gmail.com>
2023-02-19 20:46:17 +0800

    Connected WebApplication to MongoDbService and served MlbStadiumResource.END_POINT

--- application/src/main/java/sh/stefoosh/sportsdata/application/WebApplication.java
+++ application/src/main/java/sh/stefoosh/sportsdata/application/WebApplication.java
@@ -1,30 +1,35 @@
 package sh.stefoosh.sportsdata.application;
 
+import sh.stefoosh.sportsdata.model.StadiumVenue;
 import sh.stefoosh.sportsdata.repository.StadiumVenueRepository;
-import sh.stefoosh.sportsdata.service.MyService;
+import sh.stefoosh.sportsdata.service.MlbStadiumResource;
+import sh.stefoosh.sportsdata.service.MongoDbService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import sh.stefoosh.sportsdata.service.StadiumVenueResource;
+
+import java.util.List;
 
 @EnableMongoRepositories(basePackageClasses = StadiumVenueRepository.class)
 @SpringBootApplication(scanBasePackages = "sh.stefoosh.sportsdata")
 @RestController
-public class DemoApplication {
+public class WebApplication {
 
-	private final MyService myService;
+	private final MongoDbService mongoDbService;
 
-	public DemoApplication(MyService myService) {
-		this.myService = myService;
+	public WebApplication(MongoDbService mongoDbService) {
+		this.mongoDbService = mongoDbService;
 	}
 
-	@GetMapping("/")
-	public String home() {
-		return myService.message();
+	@GetMapping(MlbStadiumResource.END_POINT)
+	public List<StadiumVenue> home() {
+		return mongoDbService.stadiumVenueRepository.findAll();
 	}
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(WebApplication.class, args);
 	}
 }
---
+++ application/src/test/java/sh/stefoosh/sportsdata/application/WebApplicationTest.java
@@ -0,0 +1,23 @@
+package sh.stefoosh.sportsdata.application;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.springframework.test.context.TestPropertySource;
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@TestPropertySource(locations = "/application.properties")
+public class WebApplicationTest {
+
+//	@Autowired
+//	private MyService myService;
+//
+//	@Test
+//	public void contextLoads() {
+//		assertThat(myService.message()).isNotNull();
+//	}
+
+}
