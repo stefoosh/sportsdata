20a8f833c4c37b6b38f48c3c569404ad200a57d7
stefoosh <stephan.aleman@gmail.com>
2023-02-26 01:11:38 +0800

    Copied mongo settings to all application.properties

--- docker/setup_app_props.sh
+++ docker/setup_app_props.sh
@@ -7,6 +7,7 @@ echo
 
 echo "logging.level.org.springframework.data.mongodb=DEBUG" >> ./application/src/main/resources/application.properties
 echo "logging.level.sh.stefoosh.sportsdata.application=DEBUG" >> ./application/src/main/resources/application.properties
+echo "logging.level.org.springframework.data.mongodb.repository.query=DEBUG" >> ./application/src/main/resources/application.properties
 echo "${SPRING_DATA_MONGODB_DATABASE}" >> ./application/src/main/resources/application.properties
 echo "${SPRING_DATA_MONGODB_URI}" >> ./application/src/main/resources/application.properties
 ls -latrh ./application/src/main/resources/application.properties
@@ -19,6 +20,8 @@ echo "service.sportsDataApiBaseUrl=http://fakeApi" >> ./library/src/test/resourc
 echo "service.mlbSubscriptionKey=asdf" >> ./library/src/test/resources/application.properties
 echo "service.nhlSubscriptionKey=qwerty" >> ./library/src/test/resources/application.properties
 echo "service.soccerSubscriptionKey=1234" >> ./library/src/test/resources/application.properties
+echo "spring.data.mongodb.database=fakeDb" >> ./library/src/main/resources/application.properties
+echo "spring.data.mongodb.uri=mongodb://fakeUri" >> ./library/src/main/resources/application.properties
 ls -latrh ./library/src/test/resources/application.properties
 cat ./library/src/test/resources/application.properties
 
@@ -27,7 +30,7 @@ mkdir -p ./sync/src/main/resources
 cp ./library/src/test/resources/application.properties ./sync/src/main/resources/application.properties
 echo "spring.main.web-application-type=none" >> ./sync/src/main/resources/application.properties
 echo "spring.data.mongodb.database=fakeDb" >> ./sync/src/main/resources/application.properties
-echo "spring.data.mongodb.uri=mongodb://u:p@h:1/db" >> ./sync/src/main/resources/application.properties
+echo "spring.data.mongodb.uri=mongodb://fakeUri" >> ./sync/src/main/resources/application.properties
 ls -latrh ./sync/src/main/resources/application.properties
 cat ./sync/src/main/resources/application.properties
 echo
