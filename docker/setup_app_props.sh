0fa43d08c2fa195c9c85ff7fe52935cd94fa1a83
stefoosh <stephan.aleman@gmail.com>
2023-02-26 01:00:11 +0800

    Output application.properties for comparison

--- docker/setup_app_props.sh
+++ docker/setup_app_props.sh
@@ -4,10 +4,14 @@ pwd
 ls -latrh
 mkdir -p ./application/src/main/resources
 echo
+
 echo "logging.level.org.springframework.data.mongodb=DEBUG" >> ./application/src/main/resources/application.properties
 echo "logging.level.sh.stefoosh.sportsdata.application=DEBUG" >> ./application/src/main/resources/application.properties
 echo "${SPRING_DATA_MONGODB_DATABASE}" >> ./application/src/main/resources/application.properties
 echo "${SPRING_DATA_MONGODB_URI}" >> ./application/src/main/resources/application.properties
+ls -latrh ./application/src/main/resources/application.properties
+cat ./application/src/main/resources/application.properties
+
 echo
 mkdir -p ./library/src/test/resources
 echo "service.apiAuthHeaderKey=foo" >> ./library/src/test/resources/application.properties
@@ -15,11 +19,17 @@ echo "service.sportsDataApiBaseUrl=http://fakeApi" >> ./library/src/test/resourc
 echo "service.mlbSubscriptionKey=asdf" >> ./library/src/test/resources/application.properties
 echo "service.nhlSubscriptionKey=qwerty" >> ./library/src/test/resources/application.properties
 echo "service.soccerSubscriptionKey=1234" >> ./library/src/test/resources/application.properties
+ls -latrh ./library/src/test/resources/application.properties
+cat ./library/src/test/resources/application.properties
+
 echo
 mkdir -p ./sync/src/main/resources
 cp ./library/src/test/resources/application.properties ./sync/src/main/resources/application.properties
 echo "spring.main.web-application-type=none" >> ./sync/src/main/resources/application.properties
 echo "spring.data.mongodb.database=fakeDb" >> ./sync/src/main/resources/application.properties
 echo "spring.data.mongodb.uri=mongodb://u:p@h:1/db" >> ./sync/src/main/resources/application.properties
+ls -latrh ./sync/src/main/resources/application.properties
+cat ./sync/src/main/resources/application.properties
 echo
+
 find . -name application.properties
