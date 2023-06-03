96b71bbd2e90a1c12fcba7dc1b9857b3bd6c4492
stefoosh <stephan.aleman@gmail.com>
2023-06-03 19:12:51 +0800

    Cleaned up commented deps and lombok version interpolation

--- library/src/main/java/sh/stefoosh/sportsdata/model/Game.java
+++ library/src/main/java/sh/stefoosh/sportsdata/model/Game.java
@@ -3,6 +3,8 @@ package sh.stefoosh.sportsdata.model;
 import sh.stefoosh.sportsdata.constants.Sport;
 
 public interface Game {
+
+    // This is awesome. The subclass' lombok accessor overrides and implements the method.
     Sport getSport();
 
     int getStadiumId();
